a55e06cc4783c7c1fdb5c60e0c8a7013bebd0b83
m43i <36700796+m43i@users.noreply.github.com>
2023-06-23 12:42:42 +0200

    use copilot lua

--- lua/plugins/modules/copilot.lua
+++ lua/plugins/modules/copilot.lua
@@ -1,3 +1,44 @@
 return {
-    "github/copilot.vim",
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+        require('copilot').setup({
+            panel = {
+                enabled = false,
+                auto_refresh = false,
+                keymap = {},
+                layout = {
+                    position = "bottom", -- | top | left | right
+                    ratio = 0.4
+                },
+            },
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                debounce = 75,
+                keymap = {
+                    accept = "<M-y>",
+                    accept_word = false,
+                    accept_line = false,
+                    next = "<M-n>",
+                    prev = "<M-p>",
+                    dismiss = "<M-c>",
+                },
+            },
+            filetypes = {
+                yaml = false,
+                markdown = false,
+                help = false,
+                gitcommit = false,
+                gitrebase = false,
+                hgcommit = false,
+                svn = false,
+                cvs = false,
+                ["."] = false,
+            },
+            copilot_node_command = 'node', -- Node.js version must be > 16.x
+            server_opts_overrides = {},
+        })
+    end,
 }
