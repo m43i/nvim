88939c36fd1cc089166f8a51f2446482b6a8d97b
m43i <36700796+m43i@users.noreply.github.com>
2023-06-28 11:31:31 +0200

    feat: add telescope-fzf

--- lua/plugins/modules/telescope.lua
+++ lua/plugins/modules/telescope.lua
@@ -1,13 +1,23 @@
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.2",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-ui-select.nvim" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-ui-select.nvim",
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
     config = function()
         require("telescope").setup {
             defaults = {
                 file_ignore_patterns = { "node_modules", ".git" },
             },
             extensions = {
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = "smart_case",
+                },
                 ["ui-select"] = {
                     require("telescope.themes").get_dropdown({
                         previewer        = false,
