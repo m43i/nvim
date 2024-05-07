72df9af74c2935b7978208ebb92f6efa85ad7cc1
m43i <36700796+m43i@users.noreply.github.com>
2024-05-07 17:47:32 +0200

    refactore: neo-tree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { noremap = true, silent = true })
 
 -- move up down with cursor in middle
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -19,11 +18,5 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- paste without loosing buffer
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- format
--- vim.keymap.set({ "n", "v" }, "<leader>f", vim.lsp.buf.format)
-
 -- undotree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-
--- conform format
--- vim.keymap.set({ "n", "v" }, "<leader>cf", ":lua require('conform').format()<CR>")
--- lua/plugins/modules/neo-tree.lua
+++ lua/plugins/modules/neo-tree.lua
@@ -1,11 +1,24 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
+    event = "VimEnter",
+	cmd = "NeoTree",
+	keys = {
+		{
+			"<leader>e",
+			function()
+				require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+			end,
+		},
+	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 	},
+	deactivate = function()
+		vim.cmd([[Neotree close]])
+	end,
 	opts = {
 		filesystem = {
 			filtered_items = {
@@ -13,6 +26,7 @@ return {
 				hide_gitignored = false,
 			},
 			follow_current_file = { enabled = true },
+			use_libuv_file_watcher = true,
 		},
 	},
 }
